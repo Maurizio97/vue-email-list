7319dfd263fcfa143005ead90d17c5a4186a855b
Maurizio <proiettomaurizio97@gmail.com>
2021-11-22 15:13:00 +0100

    push

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var app = new Vue ({
     },
     mounted() {
         genEmail()
-        /* const self = this;
+        const self = this;
         // for (let i = 0; i < 10; i++)
         while (this.emailList.length <= 10){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
@@ -13,10 +13,10 @@ var app = new Vue ({
             self.emailList.push(response.data.response);
             console.log(self.emailList);
         });
-        }; */
+        };
     },
     methods: {
-        genEmail: function (){
+        /* genEmail: function (){
                 const self = this;
                 // for (let i = 0; i < 10; i++)
                 while (this.emailList.length <= 10){
@@ -26,6 +26,6 @@ var app = new Vue ({
                 console.log(self.emailList);
             });
             }
-        }
+        } */
     },
 });

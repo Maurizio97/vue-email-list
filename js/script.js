0ccea6a88736694ac62abeed3c5c569d82123a25
Maurizio <proiettomaurizio97@gmail.com>
2021-11-22 17:02:27 +0100

    update

--- js/script.js
+++ js/script.js
@@ -4,28 +4,29 @@ var app = new Vue ({
         emailList: [],
     },
     mounted() {
-        genEmail()
-        const self = this;
-        // for (let i = 0; i < 10; i++)
-        while (this.emailList.length <= 10){
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(function (response){
-            self.emailList.push(response.data.response);
-            console.log(self.emailList);
-        });
-        };
+        // const self = this;
+        // // while (this.emailList.length <= 10)
+        // for (let i = 0; i < 10; i++){
+        //     axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        //     .then(function (response){
+        //     self.emailList.push(response.data.response);
+        //     console.log(self.emailList);
+        // });
+        // };
+        this.genEmail();
+
     },
     methods: {
-        /* genEmail: function (){
+        genEmail(){
                 const self = this;
-                // for (let i = 0; i < 10; i++)
-                while (this.emailList.length <= 10){
+                /* while (this.emailList.length <= 10) */
+                for (let i = 0; i < 10; i++){
                 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then(function (response){
                 self.emailList.push(response.data.response);
                 console.log(self.emailList);
             });
             }
-        } */
+        }
     },
 });
